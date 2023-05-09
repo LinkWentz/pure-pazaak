dfda20f97085922c19196c8f926f6c1af1052c2c
Lincoln Wentz <linkwentz@gmail.com>
2023-05-08 22:26:01 -0500

    Fix point display lagging behind by a turn

--- pure_pazaak/src/Scoreboard.jsx
+++ pure_pazaak/src/Scoreboard.jsx
@@ -5,47 +5,20 @@ import { useState, useEffect } from 'react';
 
 function Scoreboard(props) {
 
-    const [colorPalette, setColorPalette] = useState({
-        "scored": "var(--scoredPointColor)",
-        "unscored": "var(--unscoredPointColor)"
-    });
-    const [pointColors, setPointColors] = useState(
-        Array.from({length: props.maxScore || 3}, () => {colorPalette.unscored})
-    );
     const [points, setPoints] = useState([]);
 
     useEffect(() => {
-        setColorPalette({
-            ...colorPalette,
-            "scored": props.scoredColor || "var(--scoredPointColor)"
-        })
-    }, [props]);
-
-    useEffect(() => {
-        setColorPalette({
-            ...colorPalette,
-            "unscored": props.unscoredColor || "var(--unscoredPointColor)"
-        })
-    }, [props]);
-
-    useEffect(() => {
-        let newPointColors = [];
-        for (const i in pointColors){
-            if (i < props.score){
-                newPointColors.push(colorPalette.scored);
+        let points = Array.from({length: props.maxScore || 3});
+        for (const i in points){
+            if (i < props.score) {
+                points[i] = <Point key={"point_" + i} color="var(--scoredPointColor)"/>
             }
             else {
-                newPointColors.push(colorPalette.unscored);
+                points[i] = <Point key={"point_" + i} color="var(--unscoredPointColor)"/>
             }
-        setPointColors(newPointColors);
-        }
-
-        let points = [];
-        for (const i in pointColors){
-            points.push(<Point key={"point_" + i} color={pointColors[i]}/>);
         }
         setPoints(points);
-    }, [props]);
+    }, [props.score]);
 
     return (
         <div className="Scoreboard">
