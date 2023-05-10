e14d2c14643a522474d353ad0411fade14731bb8
Lincoln Wentz <linkwentz@gmail.com>
2023-05-09 20:49:20 -0500

    Add copy link button to waiting screen

--- pure_pazaak/src/WaitingForOpponent.jsx
+++ pure_pazaak/src/WaitingForOpponent.jsx
@@ -1,4 +1,5 @@
 import './WaitingForOpponent.css';
+import Button from './Button';
 import { useEffect, useState } from 'react';
 
 function WaitingForOpponent(props){
@@ -14,9 +15,14 @@ function WaitingForOpponent(props){
         }
     }, [props.playerCount, props.finished]);
 
+    const CopyInviteToClipboard = () => {
+        navigator.clipboard.writeText(window.location.href);
+    };
+    
     return(
         <div className="WaitingForOpponent" style={style}>
             <p>Waiting for opponent...</p>
+            <Button onClick={CopyInviteToClipboard}>Copy Invite</Button>
         </div>
     )
 }
