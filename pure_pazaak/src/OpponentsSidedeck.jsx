9b65d11204d44b0312cc596c45867d047a3ca288
Lincoln Wentz <linkwentz@gmail.com>
2023-05-27 19:00:32 -0500

    Add + to sidedeck cards and fix sidedeck warnings

--- pure_pazaak/src/OpponentsSidedeck.jsx
+++ pure_pazaak/src/OpponentsSidedeck.jsx
@@ -1,14 +1,23 @@
 import './Sidedeck.css';
 import CardBack from './CardBack';
+import { useEffect, useState } from 'react';
 
 function Sidedeck(props) {
 
-  const card_elements = Array.from({length: props.cardsCount}, () => <CardBack/>);
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    const newCards = [];
+    for (let i = 0; i < props.cardsCount; i++){
+      newCards.push(<CardBack key={i}/>);
+    }
+    setCards(newCards);
+  }, [props.cardsCount]);
 
   return (
     <div className="OpponentsSidedeck Sidedeck">
       <div className="sidedeckGrid">
-        {card_elements}
+        {cards}
       </div>
     </div>
   )
--- pure_pazaak/src/Sidedeck.jsx
+++ pure_pazaak/src/Sidedeck.jsx
@@ -1,28 +1,32 @@
 import './Sidedeck.css';
 import Card from './Card';
+import { useEffect, useState} from 'react';
 import { useParams } from "react-router-dom";
 import { socket } from './App';
 
 function Sidedeck(props) {
 
   const params = useParams();
-  const card_elements = [];
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    const newCards = [];
+    for (const card in props.cards){
+      newCards.push(
+        <Card key={card} card={props.cards[card]} onClick={PlayCard}/>
+      );
+    }
+    setCards(newCards);
+  }, [props.cards]);
 
   const PlayCard = (card) => {
     socket.emit('game-event', card, params['roomcode']);
   };
 
-  for (const card in props.cards){
-    card_elements.push(
-      <Card key={card} card={props.cards[card]} 
-      onClick={PlayCard}/>
-    );
-  }
-
   return (
     <div className="Sidedeck">
       <div className="sidedeckGrid">
-        {card_elements}
+        {cards}
       </div>
     </div>
   )
