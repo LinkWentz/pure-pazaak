4df3358e98cc944ef906cf7e70c0413d0459c32e
Lincoln Wentz <linkwentz@gmail.com>
2023-07-04 02:18:31 -0500

    Clean up Game component

--- pure_pazaak/src/components/Game.jsx
+++ pure_pazaak/src/components/Game.jsx
@@ -19,164 +19,162 @@ import Bust from '../assets/audio/mgs_warnbust.wav';
 import WinGame from '../assets/audio/mgs_winmatch.wav';
 import WinRound from '../assets/audio/mgs_winset.wav';
 
-function Game(){
-
-    const timedQueue = (function(){
-      var API;                // internal referance to interface
-      const queue = [];       // array to hold functions
-      var task = null;        // the next task to run
-      var tHandle;            // To stop pending timeout 
-      function next(){  // runs current scheduled task and  creates timeout to schedule next
-          if(task !== null){          // is task scheduled??
-              task.func();            // run it
-              task = null;            // clear task
-          }
-          if(queue.length > 0){       // are there any remain tasks??
-              task = queue.shift();   // yes set as next task
-              tHandle = setTimeout(next,task.time) // schedual when
-          }else{
-              API.done = true;
-              setQueueDone(true);
-          }
-      }
-      return API = {
-          add : function(func,time){
-              queue.push({func : func, time: time});
-          },
-          start : function(){
-              if(queue.length > 0 && API.done){
-                  API.done = false;   // set state flag
-                  setQueueDone(false);
-                  tHandle = setTimeout(next,0);
-              }
-          },
-          clear : function(){
-              task = null;            // remove pending task
-              queue.length = 0;       // empty queue
-              clearTimeout(tHandle);  // clear timeout
-              API.done = true;        // set state flag
-              setQueueDone(true);
-          },
-          done : true,
-      }
-    })();
-
+function Game() {
     const params = useParams();
-  
-    const [opponentsUsername, setOpponentsUsername] = useState(null);
 
     const [queueDone, setQueueDone] = useState(true);
-
+    const [opponentsUsername, setOpponentsUsername] = useState(null);
     const [gameState, setGameState] = useState({
-      "boards": {
-        "you": {
-          "points": 0,
-          "standing": false,
-          "sidedeck": [],
-          "sidedeckCardPlayed": false,
-          "board": []
+        "boards": {
+            "you": {
+                "points": 0,
+                "standing": false,
+                "sidedeck": [],
+                "sidedeckCardPlayed": false,
+                "board": []
+            },
+            "opponent": {
+                "sidedeckSize": 0,
+                "points": 0,
+                "standing": false,
+                "sidedeckCardPlayed": false,
+                "board": []
+            }
         },
-        "opponent": {
-          "sidedeckSize": 0,
-          "points": 0,
-          "standing": false,
-          "sidedeckCardPlayed": false,
-          "board": []
-        }
-      },
-      "turn": "you",
-      "finished": false,
-      "playerCount": 1,
-      "role": "Player 1"
+        "turn": "you",
+        "finished": false,
+        "playerCount": 1,
+        "role": "Player 1"
     });
 
     useEffect(() => {
-      socket.emit('join-session', params['sessionName']);
-
-      socket.on('connection', () => {
         socket.emit('join-session', params['sessionName']);
-      });
-      
-      socket.on('game-state', (newGameState, label) => {
-        if (!label) {
-          label = "";
-        }
 
-        // If card is dealt
-        if (label.includes("dealt")) {
-          timedQueue.add(() => new Audio(DrawCard).play(), 0);
-        }
-        // If card is played
-        if (label.includes("played")) {
-          timedQueue.add(() => new Audio(PlayCard).play(), 0);
-        }
-        // If player wins round
-        if (label.includes("win") && label.includes("round") && label.includes(gameState["role"]) && !label.includes("No one")){
-          timedQueue.add(() => new Audio(WinRound).play(), 0);
-        }
-        // If player lsoes round
-        if (label.includes("win") && label.includes("round") && !label.includes(gameState["role"]) && !label.includes("No one")){
-          timedQueue.add(() => new Audio(LoseRound).play(), 0);
-        }
-        // if player wins game
-        if (label.includes("win") && label.includes("game") && label.includes(gameState["role"]) && !label.includes("No one")){
-          timedQueue.add(() => new Audio(WinGame).play(), 0);
-        }
-        // if player loses game
-        if (label.includes("win") && label.includes("game") && !label.includes(gameState["role"]) && !label.includes("No one")){
-          timedQueue.add(() => new Audio(LoseGame).play(), 0);
-        }
+        socket.on('connection', () => {
+            socket.emit('join-session', params['sessionName']);
+        });
 
-        timedQueue.add(() => {setGameState(newGameState)}, 0);
-        timedQueue.add(() => {}, 500);
-        timedQueue.start();
-      });
+        socket.on('game-state', (newGameState, label) => {
+            if (!label) {
+                label = "";
+            }
 
-      socket.on('username-request', () => {
-        socket.volatile.emit('username', window.localStorage.username || null);
-      });
+            // If card is dealt
+            if (label.includes("dealt")) {
+                timedQueue.add(() => new Audio(DrawCard).play(), 0);
+            }
+            // If card is played
+            if (label.includes("played")) {
+                timedQueue.add(() => new Audio(PlayCard).play(), 0);
+            }
+            // If player wins round
+            if (label.includes("win") && label.includes("round") && label.includes(gameState["role"]) && !label.includes("No one")) {
+                timedQueue.add(() => new Audio(WinRound).play(), 0);
+            }
+            // If player loses round
+            if (label.includes("win") && label.includes("round") && !label.includes(gameState["role"]) && !label.includes("No one")) {
+                timedQueue.add(() => new Audio(LoseRound).play(), 0);
+            }
+            // if player wins game
+            if (label.includes("win") && label.includes("game") && label.includes(gameState["role"]) && !label.includes("No one")) {
+                timedQueue.add(() => new Audio(WinGame).play(), 0);
+            }
+            // if player loses game
+            if (label.includes("win") && label.includes("game") && !label.includes(gameState["role"]) && !label.includes("No one")) {
+                timedQueue.add(() => new Audio(LoseGame).play(), 0);
+            }
 
-      socket.on('opponents-username', (opponentsUsername) => {
-        setOpponentsUsername(opponentsUsername);
-      });
+            timedQueue.add(() => { setGameState(newGameState) }, 0);
+            timedQueue.add(() => { }, 500);
+            timedQueue.start();
+        });
 
-      return () => {
-        socket.emit('leave-session', params['sessionName']);
+        socket.on('username-request', () => {
+            socket.volatile.emit('username', window.localStorage.username || null);
+        });
 
-        socket.off('connection');
-        socket.off('game-state');
-        socket.off('username-request');
-        socket.off('opponents-username');
-      };
+        socket.on('opponents-username', (opponentsUsername) => {
+            setOpponentsUsername(opponentsUsername);
+        });
+
+        return () => {
+            socket.emit('leave-session', params['sessionName']);
+
+            socket.off('connection');
+            socket.off('game-state');
+            socket.off('username-request');
+            socket.off('opponents-username');
+        };
     }, []);
 
+    const timedQueue = (function () {
+        var API;
+        const queue = [];
+        var task = null;
+        var tHandle;
+
+        function next() {
+            if (task !== null) {
+                task.func();
+                task = null;
+            }
+            if (queue.length > 0) {
+                task = queue.shift();
+                tHandle = setTimeout(next, task.time)
+            }
+            else {
+                API.done = true;
+                setQueueDone(true);
+            }
+        }
+
+        return API = {
+            add: function (func, time) {
+                queue.push({ func: func, time: time });
+            },
+            start: function () {
+                if (queue.length > 0 && API.done) {
+                    API.done = false;
+                    setQueueDone(false);
+                    tHandle = setTimeout(next, 0);
+                }
+            },
+            clear: function () {
+                task = null;
+                queue.length = 0;
+                clearTimeout(tHandle);
+                API.done = true;
+                setQueueDone(true);
+            },
+            done: true,
+        }
+    })();
+
     const EndTurn = () => {
-      if (queueDone){
-        socket.emit('game-event', 'end turn', params['sessionName']);
-      }
+        if (queueDone) {
+            socket.emit('game-event', 'end turn', params['sessionName']);
+        }
     };
-  
+
     const Stand = () => {
-      if (queueDone){
-        socket.emit('game-event', 'stand', params['sessionName']);
-      }
+        if (queueDone) {
+            socket.emit('game-event', 'stand', params['sessionName']);
+        }
     };
 
     const NewGame = () => {
-      if (queueDone){
-        socket.emit("game-event", "new game", params['roomcode']);
-      }
+        if (queueDone) {
+            socket.emit("game-event", "new game", params['roomcode']);
+        }
     };
 
-    return(
+    return (
         <div className="Game">
-          <GameOver finished={gameState.finished} yourScore={gameState["boards"]["you"]["points"]} 
-          opponentScore={gameState["boards"]["opponent"]["points"]} newGame={NewGame}/>
-          <WaitingForOpponent playerCount={gameState.playerCount} 
-          finished={gameState.finished}/>
-          <TurnOverlay gameState={gameState}/>
-          <GameBoard gameState={gameState} opponentsUsername={opponentsUsername}
-          endTurn={EndTurn} stand={Stand}/>
+            <GameOver finished={gameState.finished} yourScore={gameState["boards"]["you"]["points"]} 
+                opponentScore={gameState["boards"]["opponent"]["points"]} newGame={NewGame} />
+            <WaitingForOpponent playerCount={gameState.playerCount} finished={gameState.finished} />
+            <TurnOverlay gameState={gameState} />
+            <GameBoard gameState={gameState} opponentsUsername={opponentsUsername} endTurn={EndTurn} stand={Stand} />
         </div>
     )
 }
