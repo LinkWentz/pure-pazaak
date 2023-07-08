59947214fdc87c928ad76038b682afc9f5532443
Lincoln Wentz <linkwentz@gmail.com>
2023-07-08 01:51:40 -0500

    Add next function to tutorial states

--- pure_pazaak/src/components/Tutorial.jsx
+++ pure_pazaak/src/components/Tutorial.jsx
@@ -27,10 +27,61 @@ function Tutorial() {
                     "boardSum": 0
                 }
             },
+            "turn": "opponent",
+            "finished": false,
+            "playerCount": 2,
+            "role": "Player 1",
+            "next": () => {}
+        },
+        {
+            "boards": {
+                "you": {
+                    "points": 0,
+                    "standing": false,
+                    "sidedeck": [],
+                    "sidedeckCardPlayed": false,
+                    "board": [{value: 10, type: "maindeck"}],
+                    "boardSum": 0
+                },
+                "opponent": {
+                    "sidedeckSize": 0,
+                    "points": 0,
+                    "standing": false,
+                    "sidedeckCardPlayed": false,
+                    "board": [],
+                    "boardSum": 0
+                }
+            },
             "turn": "you",
             "finished": false,
             "playerCount": 2,
-            "role": "Player 1"
+            "role": "Player 1",
+            "next": () => {NextState()}
+        },
+        {
+            "boards": {
+                "you": {
+                    "points": 0,
+                    "standing": false,
+                    "sidedeck": [],
+                    "sidedeckCardPlayed": false,
+                    "board": [{value: 10, type: "maindeck"}],
+                    "boardSum": 0
+                },
+                "opponent": {
+                    "sidedeckSize": 0,
+                    "points": 0,
+                    "standing": false,
+                    "sidedeckCardPlayed": false,
+                    "board": [{value: 10, type: "maindeck"}],
+                    "boardSum": 0
+                }
+            },
+            "turn": "opponent",
+            "finished": false,
+            "playerCount": 2,
+            "role": "Player 1",
+            "next": () => {}
         }
     ]
 
@@ -40,13 +91,16 @@ function Tutorial() {
         if (currentState == tutorialStates.length - 1) {
             return;
         }
-        
         setCurrentState(currentState + 1);
     };
 
+    useEffect(() => {
+        tutorialStates[currentState]["next"]();
+    }, [currentState]);
+
     return (
         <>
-            <Game gameState={tutorialState[currentState]}
+            <Game gameState={tutorialStates[currentState]}
             opponentsUsername={"Whoever the guy is who gives the tutorial (temp)"} 
             endTurn={NextState} stand={NextState} 
             newGame={NextState} playCard={NextState}/>
