4adbe43900f31632035ea6fb9b7b6b2526f6ed46
Lincoln Wentz <linkwentz@gmail.com>
2023-07-07 22:56:50 -0500

    Add functionality for puppeteering tutorial game states

--- pure_pazaak/src/components/Tutorial.jsx
+++ pure_pazaak/src/components/Tutorial.jsx
@@ -1,15 +1,57 @@
 // Stylesheets
 import './styles/Tutorial.css';
 // Components
-import Button from './Button';
+import Game from './Game';
+// Libraries
+import { useState, useEffect } from 'react';
 
 function Tutorial() {
-  return (
-    <div className="Tutorial">
-      <header>Tutorial</header>
-      <Button to="/">Back</Button>
-    </div>
-  )
+
+    const tutorialStates = [
+        {
+            "boards": {
+                "you": {
+                    "points": 0,
+                    "standing": false,
+                    "sidedeck": [],
+                    "sidedeckCardPlayed": false,
+                    "board": [],
+                    "boardSum": 0
+                },
+                "opponent": {
+                    "sidedeckSize": 0,
+                    "points": 0,
+                    "standing": false,
+                    "sidedeckCardPlayed": false,
+                    "board": [],
+                    "boardSum": 0
+                }
+            },
+            "turn": "you",
+            "finished": false,
+            "playerCount": 2,
+            "role": "Player 1"
+        }
+    ]
+
+    const [currentState, setCurrentState] = useState(0);
+
+    const NextState = () => {
+        if (currentState == tutorialStates.length - 1) {
+            return;
+        }
+        
+        setCurrentState(currentState + 1);
+    };
+
+    return (
+        <>
+            <Game gameState={tutorialState[currentState]}
+            opponentsUsername={"Whoever the guy is who gives the tutorial (temp)"} 
+            endTurn={NextState} stand={NextState} 
+            newGame={NextState} playCard={NextState}/>
+        </>
+    )
 }
 
 export default Tutorial
