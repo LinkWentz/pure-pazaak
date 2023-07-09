cba8afd3050c0995a0f74abe6fa8f794c9535b23
Lincoln Wentz <linkwentz@gmail.com>
2023-07-09 04:04:47 -0500

    Add ability to highlight certain buttons in tutorial states

--- pure_pazaak/src/components/Tutorial.jsx
+++ pure_pazaak/src/components/Tutorial.jsx
@@ -14,7 +14,7 @@ function Tutorial() {
                 "you": {
                     "points": 0,
                     "standing": false,
-                    "sidedeck": [],
+                    "sidedeck": [1, 1, 1, 1],
                     "sidedeckCardPlayed": false,
                     "board": [],
                     "boardSum": 0
@@ -35,11 +35,42 @@ function Tutorial() {
             "bubble": {
                 "message": "This is a tutorial message bubble!"
             },
+            "highlightClass": "highlight1stCard",
+            "next": () => {}
+        },
+        {
+            "boards": {
+                "you": {
+                    "points": 0,
+                    "standing": false,
+                    "sidedeck": [1, 1, 1, 1],
+                    "sidedeckCardPlayed": false,
+                    "board": [],
+                    "boardSum": 0
+                },
+                "opponent": {
+                    "sidedeckSize": 0,
+                    "points": 0,
+                    "standing": false,
+                    "sidedeckCardPlayed": false,
+                    "board": [],
+                    "boardSum": 0
+                }
+            },
+            "turn": "you",
+            "finished": false,
+            "playerCount": 2,
+            "role": "Player 1",
+            "bubble": {
+                "message": "This is also a tutorial message bubble!"
+            },
+            "highlightClass": "highlightEndTurn",
             "next": () => {}
         }
     ]
 
     const [currentState, setCurrentState] = useState(0);
+    const [highlight, setHighlight] = useState("");
 
     const NextState = () => {
         if (currentState == tutorialStates.length - 1) {
@@ -50,16 +81,17 @@ function Tutorial() {
 
     useEffect(() => {
         tutorialStates[currentState]["next"]();
+        setHighlight(tutorialStates[currentState]["highlightClass"]);
     }, [currentState]);
 
     return (
-        <>
+        <div className={`Tutorial ${highlight}`}>
             <Game gameState={tutorialStates[currentState]}
             opponentsUsername={"Whoever the guy is who gives the tutorial (temp)"} 
             endTurn={NextState} stand={NextState} 
             newGame={NextState} playCard={NextState}/>
             <TutorialMessageBubble>{tutorialStates[currentState]["bubble"]["message"]}</TutorialMessageBubble>
-        </>
+        </div>
     )
 }
 
