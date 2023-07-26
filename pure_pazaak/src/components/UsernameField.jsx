8f660da28c86601fbbd7be32464b7cfcdc97bf26
Lincoln Wentz <linkwentz@gmail.com>
2023-07-26 18:42:29 -0500

    Default username to random selection from a list

--- pure_pazaak/src/components/UsernameField.jsx
+++ pure_pazaak/src/components/UsernameField.jsx
@@ -7,8 +7,35 @@ function UsernameField() {
 
     const [username, setUsername] = useState("");
 
+    const randomUsername = () => {
+        const defaultUsernames = [
+            'Mission Vao',
+            'Carth Onasi',
+            'T3-M4',
+            'Atton Rand',
+            'Gelrood',
+            'Niklos',
+            'Sol\'aa',
+            'Fodo Medoo',
+            'Toll Apkar',
+            'Jolan Aphett',
+            'Gonto Yas',
+            'Furko Nellis',
+            'Kudos',
+            'Suvam Tam',
+            'Mebla Dule',
+            'Doton Het',
+            'Nikko',
+            'S4-C8',
+            'Dahnis',
+            'Geredi',
+            'The Champ'
+        ];
+        return defaultUsernames[Math.round(Math.random() * (defaultUsernames.length + 1)) - 1];
+    }
+
     useEffect(() => {
-        setUsername(window.localStorage.username || Math.round(Math.random() * 10**8).toString());
+        setUsername(window.localStorage.username || randomUsername());
       }, []);
 
     useEffect(() => {
