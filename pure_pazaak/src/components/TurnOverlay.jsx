b3f4b1808ad5de7ad4bedd8ce281c99dea8b8ee8
Lincoln Wentz <linkwentz@gmail.com>
2023-07-26 22:13:16 -0500

    Make overlay behaviors consistent

--- pure_pazaak/src/components/TurnOverlay.jsx
+++ pure_pazaak/src/components/TurnOverlay.jsx
@@ -3,15 +3,28 @@ import './styles/TurnOverlay.css';
 // Components
 import Overlay from './Overlay';
 
-function TurnOverlay(props){
+function TurnOverlay({ gameState, inputDisabled }){
+
+    const you = {
+        turn: gameState["turn"] == 'you',
+        standing: gameState["boards"]["you"]["standing"],
+        cardPlayed: gameState["boards"]["you"]["sidedeckCardPlayed"]
+    }
+
+    const opponent = {
+        turn: gameState["turn"] == 'opponent',
+        standing: gameState["boards"]["opponent"]["standing"],
+        cardPlayed: gameState["boards"]["opponent"]["sidedeckCardPlayed"]
+    }
+
     return(
         <div className="TurnOverlay">
-            <Overlay className="yourTurnOverlay" visible={props.gameState.turn != 'you' && !props.gameState["boards"]["you"]["standing"]}/>
-            <Overlay className="opponentsTurnOverlay" visible={props.gameState.turn != 'opponent' && !props.gameState["boards"]["opponent"]["standing"]}/>
-            <Overlay className="yourTurnOverlay" visible={props.gameState["boards"]["you"]["standing"]}>Standing</Overlay>
-            <Overlay className="opponentsTurnOverlay" visible={props.gameState["boards"]["opponent"]["standing"]}>Standing</Overlay>
-            <Overlay className="yourCardPlayedOverlay" visible={(props.gameState["boards"]["you"]["sidedeckCardPlayed"] || props.inputDisabled) && props.gameState.turn == 'you'}/>
-            <Overlay className="opponentsCardPlayedOverlay" visible={props.gameState["boards"]["opponent"]["sidedeckCardPlayed"] && props.gameState.turn == 'you'}/>
+            <Overlay className="yourTurnOverlay" visible={!you.turn && !you.standing}/>
+            <Overlay className="opponentsTurnOverlay" visible={!opponent.turn && !opponent.standing}/>
+            <Overlay className="yourTurnOverlay" visible={you.standing}>Standing</Overlay>
+            <Overlay className="opponentsTurnOverlay" visible={opponent.standing}>Standing</Overlay>
+            <Overlay className="yourCardPlayedOverlay" visible={you.cardPlayed || !you.turn || you.standing || inputDisabled}/>
+            <Overlay className="opponentsCardPlayedOverlay" visible={opponent.cardPlayed || !opponent.turn || opponent.standing}/>
         </div>
     )
 }
